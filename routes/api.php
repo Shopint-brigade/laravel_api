23ab4c520fadbdb53b7c578e381bdc201be1ff3d
Nart <info@shopint-brigade.com>
2022-02-02 00:30:07 +0300

    test api

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+Route::get('/test', function(){
+    return ['message' => 'hello'];
+});
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
